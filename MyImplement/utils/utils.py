f5e2a90e209e1a2a9bffd72b3c367ef3197c1e4a
xansar <xxzhang16@fudan.edu.cn>
2022-08-05 14:41:08 +0800

    Criteo数据集，LR模型，以及CTR预估的metric

--- MyImplement/utils/utils.py
+++ MyImplement/utils/utils.py
@@ -1,13 +1,20 @@
 import numpy as np
 import pandas as pd
+import pandas_profiling as pp
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
+from category_encoders.target_encoder import TargetEncoder
+from category_encoders.one_hot import OneHotEncoder
 
 import torch
+import torch.utils.data
+from torch.utils.data import Dataset
 
 from collections import Counter
+import os
 
 
-class Dataset:
+class MyDataset:
     def __init__(self):
         self.device = torch.device('cpu')
         self.data: pd.DataFrame = None
@@ -36,7 +43,7 @@ class Dataset:
         return feild_dims, (train_X, train_Y), (valid_X, valid_Y), (test_X, test_Y)
 
 
-class MovieLensDataset(Dataset):
+class MovieLensDataset(MyDataset):
     def __init__(self, file, read_part=True, sample_number=100000, task='classification'):
         super(MovieLensDataset, self).__init__()
 
@@ -57,6 +64,83 @@ class MovieLensDataset(Dataset):
             data_df['rating'] = data_df.apply(lambda x: 1 if x['rating'] > 3.0 else 0, axis=1)
 
         self.data = data_df.values
+
+
+class CriteoDataset(Dataset):
+    def __init__(self, pth, mode='train', encoders=None, show_info=False):
+        self.pth = pth
+        self.mode = mode
+        self.encoders = encoders
+        super(CriteoDataset, self).__init__()
+        dtype = {
+            'label': np.int32,
+        }
+        for i in range(13):
+            dtype[f'I_{i + 1}'] = np.float32
+        for i in range(26):
+            dtype[f'C_{i + 1}'] = str
+
+        with open(pth, 'r', encoding='utf-8') as file:
+            data_df = pd.read_csv(file, sep='\t', dtype=dtype)
+            data_df.drop(columns=['Unnamed: 0'], axis=1, inplace=True)
+
+        self.data_df: pd.DataFrame = data_df
+        self.encoders = self._pre_process()
+        self.data_X = torch.tensor(self.data_df.values[:, 1:], dtype=torch.float32)
+        self.data_y = torch.tensor(self.data_df.values[:, 0], dtype=torch.float32)
+        if show_info:
+            print(self.data_df.info())
+            print(self.data_df.head())
+
+    def _generate_report(self):
+        report_pth = os.path.split(self.pth)[0] + '/' + f'Criteo-{self.mode}-report.html'
+        if not os.path.exists(report_pth):
+            report = pp.ProfileReport(self.data_df)
+            report.to_file(report_pth)
+
+    def __getitem__(self, index):
+        return self.data_y[index], self.data_X[index]
+
+    def __len__(self):
+        return self.data_X.shape[0]
+
+    def _pre_process(self):
+        # 要注意一点，这里想要使用的目标编码方式是有监督的，或者是利用了全局的统计信息，这就牵扯到了数据集划分的问题
+        # 应该只利用训练集上的监督信息对其进行编码，所以需要在预处理前先划分好三个数据集
+        one_hot_columns = [f'C_{i}' for i in [6, 9, 14, 17, 20, 22, 23]]
+        target_columns = [f'C_{i}' for i in range(1, 27) if i not in [6, 9, 14, 17, 20, 22, 23]]
+        if self.encoders is None:
+            # 数值型特征使用均值填充
+            mean_fillna = dict()
+            for i in range(1, 14):
+                m = self.data_df[f'I_{i}'].mean()
+                mean_fillna[f'I_{i}'] = m
+                self.data_df[f'I_{i}'] = self.data_df[f'I_{i}'].fillna(m)
+
+            # 根据分析报告，c6 c9 c14 c17 c20 c22 c23 这些特征类型数量在20以内，使用onehot编码
+            one_hot_encoder = OneHotEncoder(cols=one_hot_columns, handle_unknown='value', handle_missing='value').fit(
+                self.data_df)
+            self.data_df = one_hot_encoder.transform(self.data_df)
+
+            target_encoder = TargetEncoder(cols=target_columns, handle_unknown='value', handle_missing='value').fit(
+                self.data_df.drop(columns=['label']), self.data_df['label'])
+            label = self.data_df['label']
+            self.data_df = target_encoder.transform(self.data_df.drop(columns=['label']))
+            self.data_df = pd.concat([label, self.data_df], axis=1)
+
+            return [mean_fillna, one_hot_encoder, target_encoder]
+        else:
+            mean_fillna = self.encoders[0]
+            for i in range(1, 14):
+                m = mean_fillna[f'I_{i}']
+                self.data_df[f'I_{i}'] = self.data_df[f'I_{i}'].fillna(m)
+
+            one_hot_encoder = self.encoders[1]
+            target_encoder = self.encoders[2]
+            self.data_df = one_hot_encoder.transform(self.data_df)
+            label = self.data_df['label']
+            self.data_df = target_encoder.transform(self.data_df.drop(columns=['label']))
+            self.data_df = pd.concat([label, self.data_df], axis=1)
 
 
 class Metric:
@@ -202,24 +286,95 @@ class Metric:
         self._MRR(label, pred)
 
 
+class CTRMetric:
+    def __init__(self):
+        self.metric_dict = dict()
+        self.init_metric()
+
+    def init_metric(self):
+        self.metric_dict = {
+            'accuracy': 0.0,
+            'precision': 0.0,
+            'recall': 0.0,
+            'F1': 0.0,
+            'AUC': 0.0,
+            'cnt': 0,
+        }
+
+    def compute_metric(self, pred: torch.Tensor, label: torch.Tensor, threshold=0.5):
+        pred = pred.detach().cpu().numpy().flatten()
+        label = label.detach().cpu().numpy().flatten()
+
+        self.metric_dict['AUC'] += roc_auc_score(label, pred)
+
+        pred = pred > threshold
+        label = label == 1
+
+        TP = np.sum(pred * label)
+        TN = np.sum((~pred) * (~label))
+        FP = np.sum(pred * (~label))
+        FN = np.sum((~pred) * label)
+
+        T = np.sum(label)
+        F = label.shape[0] - T
+
+        self.metric_dict['accuracy'] += (TP + TN) / (T + F)
+        P = TP / (TP + FP) if TP + FP != 0 else 0
+        self.metric_dict['precision'] += P
+        R = TP / (TP + FN) if TP + FN != 0 else 0
+        self.metric_dict['recall'] += R
+        self.metric_dict['F1'] += 2 / (1 / P + 1 / R) if P * R != 0 else 0
+
+        self.metric_dict['cnt'] += 1
+
+    def get_batch_metric(self):
+        cnt = self.metric_dict['cnt']
+        for k, v in self.metric_dict.items():
+            if k == 'cnt':
+                continue
+            self.metric_dict[k] = v / cnt
+
+class Logger:
+    def __init__(self):
+        pass
+
+    def log(self):
+        pass
+
+    def __str__(self):
+        pass
+
+
 def dataset_test():
-    with open('../../dataset/ml-latest-small-ratings.txt', 'r', encoding='utf-8') as f:
-        ml_dataset = MovieLensDataset(f)
-        print(ml_dataset.data[:10, :])
-        print(type(ml_dataset.data))
-    field_dims, train_set, valid_set, test_set = ml_dataset.train_valid_test_split(train_size=0.8, valid_size=0.1,
-                                                                                   test_size=0.1)
-    print(f'train_set shape: {train_set[0].shape}')
-    print(f'valid_set shape: {valid_set[0].shape}')
-    print(f'test_set shape: {test_set[0].shape}')
+    # MovieLens
+    # with open('../../dataset/ml-latest-small-ratings.txt', 'r', encoding='utf-8') as f:
+    #     ml_dataset = MovieLensDataset(f)
+    #     print(ml_dataset.data[:10, :])
+    #     print(type(ml_dataset.data))
+    # field_dims, train_set, valid_set, test_set = ml_dataset.train_valid_test_split(train_size=0.8, valid_size=0.1,
+    #                                                                                test_size=0.1)
+    # print(f'train_set shape: {train_set[0].shape}')
+    # print(f'valid_set shape: {valid_set[0].shape}')
+    # print(f'test_set shape: {test_set[0].shape}')
+
+    # Criteo
+    train_pth = '../../dataset/criteo-100k-train.txt'
+    test_pth = '../../dataset/criteo-100k-test.txt'
+    train_set = CriteoDataset(train_pth, mode='train')
+
+    encoders = train_set.encoders
+
+    test_set = CriteoDataset(test_pth, mode='test', encoders=encoders)
+
+    print(test_set[123])
 
 
 def metric_test():
     metric = Metric()
     label = [
-        [3,2,1],
+        [3, 2, 1],
         [],
-        [1,3,2]
+        [1, 3, 2]
     ]
     pred = np.array([
         [3, 2, 1],
@@ -232,5 +387,74 @@ def metric_test():
     print(metric.metric_dict)
 
 
+def read_and_split_data(pth, read_part=False, sample_number=100000, size=None, random_state=2022):
+    if size is None:
+        size = [0.8, 0.1, 0.1]
+
+    names = [
+        'label',
+    ]
+    dtype = {
+        'label': np.int32,
+    }
+    for i in range(13):
+        names.append(f'I_{i + 1}')
+        dtype[f'I_{i + 1}'] = np.float32
+    for i in range(26):
+        names.append(f'C_{i + 1}')
+        dtype[f'C_{i + 1}'] = str
+
+    train_size = size[0]
+    valid_size = size[1]
+    test_size = size[2]
+
+    with open(pth, 'r', encoding='utf-8') as file:
+        if read_part:
+            data_df = pd.read_csv(file, sep='\t', names=names, dtype=dtype, nrows=sample_number)
+        else:
+            data_df = pd.read_csv(file, sep='\t', names=names, dtype=dtype)
+
+    train_df = data_df.sample(frac=train_size, random_state=random_state, axis=0)
+    valid_test = data_df[~data_df.index.isin(train_df.index)]
+
+    valid_size = valid_size / (valid_size + test_size)
+    valid_df = valid_test.sample(frac=valid_size, random_state=random_state, axis=0)
+    test_df = valid_test[~valid_test.index.isin(valid_df.index)]
+
+    print(test_df.info())
+    print(test_df.head())
+    save_pth = os.path.split(pth)[0]
+    train_save_pth = save_pth + '/criteo-100k-train.txt'
+    valid_save_pth = save_pth + '/criteo-100k-valid.txt'
+    test_save_pth = save_pth + '/criteo-100k-test.txt'
+
+    train_df.to_csv(train_save_pth, sep='\t')
+    valid_df.to_csv(valid_save_pth, sep='\t')
+    test_df.to_csv(test_save_pth, sep='\t')
+
+
+def ctr_metric_test():
+    pred = torch.tensor(
+        [[0.8, 0.7, 0.5, 0.5, 0.5, 0.5, 0.3]],
+        dtype=torch.float32,
+        device='mps'
+    )
+    label = torch.tensor(
+        [[1, 1, 0, 0, 1, 1, 0]],
+        dtype=torch.float32,
+        device='mps'
+    )
+
+    ctr_metric = CTRMetric()
+    ctr_metric.compute_metric(pred, label)
+    print(ctr_metric.metric_dict)
+
+    ctr_metric.compute_metric(pred, label)
+    ctr_metric.get_batch_metric()
+    print(ctr_metric.metric_dict)
+
+
+
 if __name__ == '__main__':
-    metric_test()
+    # read_and_split_data('../../dataset/criteo-100k.txt')
+    ctr_metric_test()
